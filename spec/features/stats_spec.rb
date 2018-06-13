ceb41ccf5fedd49476a2015a78f997deb9ae0b91
Rad Batnag <radamanthus@gmail.com>
2018-06-13 13:32:21 +0800

    Added failing spec for stats feature

---
+++ spec/features/stats_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "Stats", type: :feature do
+  scenario "URL visits are logged and collated" do
+    u = Url.create(url:'goo.gl')
+    3.times do
+      visit "/#{u.shortened_url}"
+    end
+
+    visit "/stats/#{u.shortened_url}"
+
+    parsed_body = JSON.parse(response.body)
+    expect(parsed_body['total_hits']).to equal(3)
+  end
+
+end
