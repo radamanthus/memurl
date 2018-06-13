2e4327508a15e09ee5524e205f856534cf5231a1
Rad Batnag <radamanthus@gmail.com>
2018-06-13 09:59:36 +0800

    Added unit test for Url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,13 @@
 class Url < ApplicationRecord
+  before_create :append_default_protocol
   after_create :generate_short_url
+
+  def append_default_protocol
+    unless self.url.start_with?(Settings.default_protocol)
+      url_with_default_protocol = Settings.default_protocol + "://" + self.url
+      self.url = url_with_default_protocol
+    end
+  end
 
   def generate_short_url
     shortened_url = UrlShortener.shorten(self.id)
