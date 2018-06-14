ceb733c746ca3791b749693482ef2be465f2155d
Rad Batnag <radamanthus@gmail.com>
2018-06-14 12:21:30 +0800

    removed unused method in stats_controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -10,19 +10,4 @@ class StatsController < ApplicationController
       render 'home/404'
     end
   end
-
-  def redirect
-    url = Url.where(shortened_url: params[:shorturl]).first
-    if url
-      url.logs.create(
-        remote_ip: request.remote_ip,
-        user_agent: request.headers['HTTP_USER_AGENT'],
-        referer: request.referer
-      )
-      redirect_to url.url
-    else
-      @shorturl = params[:shorturl]
-      render '404'
-    end
-  end
 end
