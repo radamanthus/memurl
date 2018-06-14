627ed0b3d7d984d208b9772b7e975fd42179c35a
Rad Batnag <radamanthus@gmail.com>
2018-06-14 17:47:39 +0800

    Add all hits to stats result

--- app/models/url_stats.rb
+++ app/models/url_stats.rb
@@ -16,9 +16,19 @@ class UrlStats
       short_url: url.shortened_url,
       created_at: url.created_at,
       total_hits: total_hits,
-      browser_stats: {},
-      os_stats: {}
+      hits: []
     }
+
+    stats[:hits] = url.logs.map do |l|
+      {
+        ip: l.remote_ip,
+        user_agent: l.user_agent,
+        browser: l.browser,
+        os: l.os,
+        referer: l.referer,
+        timestamp: l.created_at
+      }
+    end
 
     stats
   end
